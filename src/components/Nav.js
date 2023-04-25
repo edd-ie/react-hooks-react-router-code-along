a3afbc223d7e580d291ab5e9cbf41a8f32b0af80
_Edd.ie_ <57677657+edd-ie@users.noreply.github.com>
2023-04-25 12:34:23 +0300

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,17 +15,7 @@ export default function Nav() {
 
   return (
     <div>
-      <NavLink
-        to="/"
-        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-        exact
-        /* add styling to Navlink */
-        style={linkStyles}
-        /* add prop for activeStyle */
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
+      <NavLink to="/" exact style={linkStyles} activeStyle={{background: "darkblue",}}>
         Home
       </NavLink>
       <NavLink
